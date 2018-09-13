63840673bb3d9ed4671b0bc3b6ad80028f9dd8fa
baseem <baseem@gmail.com>
2018-09-12 20:20:30 -1000

    project update

--- screens/OptionsScreen.js
+++ screens/OptionsScreen.js
@@ -9,59 +9,8 @@ import {
   View,
 } from 'react-native';
 import { WebBrowser, Camera, Permissions, Location, FileSystem } from 'expo';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
-const debrisStock = 'http://www.solomonstarnews.com/media/k2/items/cache/9f7bea46670ffcc656accfb2e282ded1_XL.jpg';
-const sealStock = "https://assets.atlasobscura.com/article_images/58631/image.jpg";
-
-
-const getPinColor = type => {
-  if(type === 'seal'){
-    return 'blue';
-  }
-  else if(type === 'debris'){
-    return 'red';
-  }
-  else{
-    return 'yellow';
-  }
-};
-
-const getStockImg = type => {
-  if(type === 'seal'){
-    return sealStock;
-  }
-  else if(type === 'debris'){
-    return debrisStock;
-  }
-  else{
-    return debrisStock;
-  }
-};
-
-
-const createLocations = photos => {
-  let locales = [];
-  let poi;
-  photos.map(e =>{
-    let arr = e.split('&');
-    let date = arr[0];
-    let lat = arr[1];
-    let lon = arr[2];
-    let type = arr[3].split('.jpg')[0];
-    poi = {
-      lat: Number(lat),
-      lon: Number(lon),
-      date: Date(date),
-      type: type,
-      pinColor: getPinColor(type),
-      src: getStockImg(type)
-    }
-    locales.push(poi);
-  });
-  return locales;
-};
-
-const PHOTOS_DIR = FileSystem.documentDirectory + 'photosA';
 
 export default class OptionsScreen extends React.Component {
 	state = {
@@ -70,33 +19,12 @@ export default class OptionsScreen extends React.Component {
 	}
 
   static navigationOptions = {
-    title: 'Notifications',
+    title: 'Our Cleanup Stats...',
   };
 
 
-  componentDidMount = async () => {
-    this.refreshImages();
-  }
 
-  refreshImages = async () => {
-    console.log(this.state.locations);
-    const photos = await FileSystem.readDirectoryAsync(PHOTOS_DIR);
-    this.setState({
-      photos: photos,
-      locations: createLocations(photos)
-    });
-  };
 
-  renderNotifications(){
-    return (this.state.locations.map((e,i) => {
-      return (
-        <View key={i} style={styles.notificationContainer}>
-    			<Image source={{uri: e.src, width: 60, height: 60}} />
-    			<Text style={styles.notificationTextDate}>{e.date.toString()}</Text>
-    		</View>
-      )
-    }))
-  }
 
 
 
@@ -104,8 +32,42 @@ export default class OptionsScreen extends React.Component {
   render() {
 
     return (
-    	<ScrollView>
-    		{this.renderNotifications()}   
+    	<ScrollView style={styles.container}>
+        <View style={styles.statsContainer}>
+    		 <AnimatedCircularProgress
+          size={100}
+          width={15}
+          fill={0}
+          tintColor="blue"
+          backgroundColor="red">
+            {
+              (fill) => (
+                <Text style={styles.points}>
+                  91
+                </Text>
+              )
+            }
+          </AnimatedCircularProgress>
+          <Text>Total Clean-Ups</Text>
+        </View>
+        <View style={styles.statsContainer}>
+         <AnimatedCircularProgress
+          size={100}
+          width={15}
+          fill={0}
+          tintColor="#3d5875"
+          backgroundColor="lightblue">
+            {
+              (fill) => (
+                <Text style={styles.points}>
+                  19,583
+                </Text>
+              )
+            }
+          </AnimatedCircularProgress>
+          <Text>Volunteers</Text>
+        </View>
+
       </ScrollView>
 
 
@@ -118,16 +80,18 @@ export default class OptionsScreen extends React.Component {
 
 
 const styles = StyleSheet.create({
-  notificationContainer: {
+  container: {
     flex: 1,
     backgroundColor: '#fff',
-    flexDirection: 'row',
-    padding: 10,
+    flexDirection: 'row'
   },
-  notificationTextDate: {
-  	padding: 5,
-  	color: 'grey'
-  }
+  statsContainer: {
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    padding: 20
+  },
+
 
 
 });
+
