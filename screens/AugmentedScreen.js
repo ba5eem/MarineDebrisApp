cfebdb076a80e4bc471003b216ce62a03d43f092
baseem <baseem@gmail.com>
2018-09-17 11:02:31 -1000

    project update

--- screens/AugmentedScreen.js
+++ screens/AugmentedScreen.js
@@ -1,109 +1,103 @@
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import { GLView, WebBrowser, Camera, Permissions, Location, FileSystem } from 'expo';
-
-import ExpoTHREE from 'expo-three';
-
-console.disableYellowBox = true;
-
-// SERVER SETTINGS:
-
+import { AR } from 'expo';
+// Let's alias ExpoTHREE.AR as ThreeAR so it doesn't collide with Expo.AR.
+import ExpoTHREE, { AR as ThreeAR, THREE } from 'expo-three';
+// Let's also import `expo-graphics`
+// expo-graphics manages the setup/teardown of the gl context/ar session, creates a frame-loop, and observes size/orientation changes.
+// it also provides debug information with `isArCameraStateEnabled`
+import { View as GraphicsView } from 'expo-graphics';
 
 export default class AugmentedScreen extends React.Component {
-
-  state = {
-    hasCameraPermission: null,
-    type: Camera.Constants.Type.back,
-    location: null,
-    errorMessage: null,
-  };
-
-  static navigationOptions = {
-    header: null,
-  };
-
-
-
-  async componentWillMount() {
-    const { status } = await Permissions.askAsync(Permissions.CAMERA);
-    this.setState({ hasCameraPermission: status === 'granted' });
-
-    if (Platform.OS === 'android' && !Constants.isDevice) {
-      this.setState({
-        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
-      });
-    } else {
-      this._getLocationAsync();
-    }
+  componentDidMount() {
+    // Turn off extra warnings
+    THREE.suppressExpoWarnings()
   }
-
-  _getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== 'granted') {
-      this.setState({
-        errorMessage: 'Permission to access location was denied',
-      });
-    }
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ location }); // get iPhone location
-  };
-
-
-
-
-
-
-
+  
   render() {
-
-      return (<View></View>);
+    // You need to add the `isArEnabled` & `arTrackingConfiguration` props.
+    // `isArRunningStateEnabled` Will show us the play/pause button in the corner.
+    // `isArCameraStateEnabled` Will render the camera tracking information on the screen.
+    // `arTrackingConfiguration` denotes which camera the AR Session will use. 
+    // World for rear, Face for front (iPhone X only)
+    return (
+      <GraphicsView
+        style={{ flex: 1 }}
+        onContextCreate={this.onContextCreate}
+        onRender={this.onRender}
+        onResize={this.onResize}
+        isArEnabled
+        isArRunningStateEnabled
+        isArCameraStateEnabled
+        arTrackingConfiguration={AR.TrackingConfigurations.World}
+      />
+    );
   }
 
+  // When our context is built we can start coding 3D things.
+  onContextCreate = async ({ gl, scale: pixelRatio, width, height }) => {
+    // This will allow ARKit to collect Horizontal surfaces
+    AR.setPlaneDetection(AR.PlaneDetectionTypes.Horizontal);
+
+    // Create a 3D renderer
+    this.renderer = new ExpoTHREE.Renderer({
+      gl,
+      pixelRatio,
+      width,
+      height,
+    });
+
+    // We will add all of our meshes to this scene.
+    this.scene = new THREE.Scene();
+    // This will create a camera texture and use it as the background for our scene
+    this.scene.background = new ThreeAR.BackgroundTexture(this.renderer);
+    // Now we make a camera that matches the device orientation. 
+    // Ex: When we look down this camera will rotate to look down too!
+    this.camera = new ThreeAR.Camera(width, height, 0.01, 1000);
+    // Make a cube - notice that each unit is 1 meter in real life, we will make our box 0.1 meters
+    const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
+    // Simple color material
+    const material = new THREE.MeshPhongMaterial({
+      color: "green",
+    });
+    const material2 = new THREE.MeshPhongMaterial({
+      color: "red",
+    });
+    
+    // Combine our geometry and material
+    this.cube = new THREE.Mesh(geometry, material);
+    // Place the box 0.4 meters in front of us.
+    this.cube.position.z = -0.9
+    this.cube.position.x = 0.2
+
+    // Combine our geometry and material
+    this.cube2 = new THREE.Mesh(geometry, material2);
+    // Place the box 0.4 meters in front of us.
+    this.cube2.position.z = -0.9
+    this.cube2.position.x = 0
+
+
+    // Add the cube to the scene
+    this.scene.add(this.cube2);
+    // Setup a light so we can see the cube color
+    // AmbientLight colors all things in the scene equally.
+    this.scene.add(new THREE.AmbientLight(0xffffff));
+  };
+
+  // When the phone rotates, or the view changes size, this method will be called.
+  onResize = ({ x, y, scale, width, height }) => {
+    // Let's stop the function if we haven't setup our scene yet
+    if (!this.renderer) {
+      return;
+    }
+    this.camera.aspect = width / height;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setPixelRatio(scale);
+    this.renderer.setSize(width, height);
+  };
+
+  // Called every frame.
+  onRender = () => {
+    // Finally render the scene with the AR Camera
+    this.renderer.render(this.scene, this.camera);
+  };
 }
-
-
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  viewOfPhotoContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginBottom: 20,
-    opacity: 0.9
-  },
-  takePhotoContainer: {
-    flex: 0.3,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-    borderRadius: 400,
-    backgroundColor: "#fefefe",
-    marginRight: 20,
-    marginLeft: 20,
-  },
-  takePhotoButton: { 
-    fontSize: 23, 
-    color: '#2f95dc' , 
-    padding:5  
-  },
-  takePhotoButtonSeal: { 
-    fontSize: 23, 
-    color: 'red' , 
-    padding:5  
-  },
-
-});
